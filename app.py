646d3c17287e66222a248bad087c5e58993bd2db
Aditya Rajmane <anr331@nyu.edu>
2015-05-11 20:02:15 -0400

    v2

--- app.py
+++ app.py
@@ -12,27 +12,40 @@ MONGODB_HOST = 'localhost'
 MONGODB_PORT = 27017
 DBS_NAME = 'test'
 COLLECTION_NAME = 'accern'
-FIELDS = {'harvested_at':True,"source_id":True,'entities.ticker':True, "event_source_rank":True,"article_url":True,"_id":False}
+FIELDS = {'harvested_at':True,"overall_source_rank": True,"event_groups.group":True,"source_id":True,'entities.ticker':True, "event_source_rank":True,"article_url":True,"_id":False}
 start = datetime(2012, 9, 25, 0, 00, 00)
+end = datetime(2012, 10, 25, 1, 00, 00)
 print start
 # @app.route("/")
 # def index():
 #     return render_template("index.html")
 @app.route("/")
 def index():
+        return render_template("index.html")
+
+@app.route("/v1")
+def v1():
         return render_template("v1.html")
+
+@app.route("/v2Try")
+def v2Try():
+        return render_template("v2Try.html")
 
 @app.route("/serve/")
 def test_projects():
     connection = Connection(MONGODB_HOST, MONGODB_PORT)
     collection = connection[DBS_NAME][COLLECTION_NAME]
-    projects = collection.find({"harvested_at" : {"$gte": start}},fields=FIELDS).limit(100000)
+    projects = collection.find({"harvested_at" : {"$gte": start,"$lte":end},"entities.ticker":{"$in":["IBM","AXP","AAPL","BA","CAT","CSCO","CVX","DD","DIS","GE","GS","HD","INTC","JNJ","JPM","KO","MCD","MMM","MRK","MSFT","NKE","PFE","PG","TRV","UNH","UTX","V","VZ","WMT","XOM"]}},fields=FIELDS).sort("harvested_at",-1)
+    
     json_projects = []
     for project in projects:
         json_projects.append(project)
     json_projects = json.dumps(json_projects, default=json_util.default)
     connection.disconnect()
-    return json_projects
+    f = open( 'data.json', 'w')
+    f.write(json_projects)
+    f.close()
+    return json_projects    
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8181,debug=True)
 
---
+++ static/lib/js/v2.js
@@ -0,0 +1,154 @@
+function prepareAllEvents(eventsList){
+
+	/*currently for single events we use this:
+		{date: '', sourceRank: , sourceId: , articleURL}
+
+		Making a dictionary
+		
+		Broad level dictionary, collects all data at once. Contains: Event Groups + Ticker value combination
+		<event group, [{date: '', sourceRank: "", sourceId: "", articleURL: ""}, {date: '', sourceRank: "", sourceId: "", articleURL: ""}] >
+
+		Event level dictionary, Collects only when required. Conatins: specific event  + ticker value combination
+		<event name, [{date: '', sourceRank: "", sourceId: "", articleURL: ""}, {date: '', sourceRank: "", sourceId: "", articleURL: ""}] >
+	*/
+
+	var dataHighMap = {};
+	var tempDataMap = [];
+	
+	_.each(masterData, function(root, index, list){
+		_.each(root.entities, function(entity, index, list){
+
+			if(entity.ticker == srch){//Collect only searched company's events
+
+				var dataMap = { //Common attributes
+					date: new Date(root.harvested_at.$date)
+					, sourceRank: 	root.overall_source_rank
+					, sourceId: root.source_id
+					, articleURL: root.article_url
+				};
+
+				_.each(root.event_groups, function(eventGroup, index, list){
+
+					if(dataHighMap[eventGroup.group] == undefined){
+						//Group does not exist in the dictionary
+						dataHighMap[eventGroup.group] = [];
+					}
+
+					dataHighMap[eventGroup.group].push(dataMap);
+				});
+			}
+		});/*--Root ends--*/
+
+	});/*--Main master data _.each ends--*/
+
+	//console.log(dataHighMap["Business Concerns"]); //["Accounting Actions"][0][0].date
+	//console.log(dataMap);
+
+	drawAllEvents(dataHighMap);
+}
+
+
+function drawAllEvents(dataHighMap){
+
+	$("#pnlPlot").empty();
+
+	console.log("Now plotting v2.0");
+
+	//console.log(temp);
+	var uniqDates = [];
+
+	//Collect unique dates
+	_.each(dataHighMap, function(root, index, list){
+		_.each(root, function(element, eIndex, eList){
+
+			var d =  new Date(element.date);
+			d.setHours(0,0,0);
+			uniqDates.push(d);
+		});
+	});
+
+	//Filtering out unique 
+	uniqDates = _.uniq(uniqDates, function(d){ return d.setHours(0,0,0); });
+
+	//console.log(uniqDates);
+	
+	var filterPrice = [];
+	 _.each(priceData[srch], function(d){
+		_.each(uniqDates, function(element){
+			//console.log(element);
+			/*console.log(new Date(d.date));
+			console.log("vs");
+			console.log(element);*/
+			if(element.getTime() == new Date(d.date).getTime()){
+				
+				filterPrice.push(d);
+			}
+		})
+	});
+
+	//console.log("filter");
+	//console.log(filter);
+
+	_.each(dataHighMap, function(root, index, list){
+
+		//Join Unique dates and Event data on dates to get acurarte price values
+		/*_.each(root, function(element, eIndex, eList){
+			//console.log(element.date);
+
+			var d = new Date(element.date);
+
+			d.setHours(0,0,0);
+
+			var singlePrice = _.find(priceData, function(d){
+
+					});
+
+
+		});*/
+
+		drawSingleScatter(root, index, filterPrice)
+	});
+}
+
+function getPriceDataAndPrepareEvents(){
+
+	//Get CSV Data
+	d3.csv("./static/PriceData/" + srch + ".csv", function(error, data){
+		if(error){
+			//Erorr occured
+			console.log(error);
+		}
+		else{
+			//console.log(data);
+			
+			_.each(data, function(element, index, list){
+
+				if(element.Change.toLowerCase() != "closed" && element.Change.toLowerCase() != ""){
+
+					//console.log(element.Date);
+					var tempPriceData = {
+						date: new Date(element.Date)
+						, change: element.Change
+					};
+
+					//console.log(tempPriceData);
+
+					if(priceData[srch] == undefined){
+						//Group does not exist in the dictionary
+						priceData[srch] = [];
+					}
+
+					priceData[srch].push(tempPriceData);
+				}
+								
+			});
+
+			//console.log(priceData);
+
+			//Populate event groups to select from
+    			makeAList(events);
+		}
+
+
+	});
+}
